8aaceba06b35492a7513fdc09561b628e8aa0068
Hana Fauziah Hanum <hanaanumnnum@gmail.com>
2025-05-06 14:33:28 +0700

    Update Tasks

---
+++ assets/script/index.js
@@ -0,0 +1,13 @@
+function getData(event){
+    event.preventDefault()
+
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let subjectChoose = document.getElementById("subject")
+    let subject = subjectChoose.options[subjectChoose.selectedIndex].text
+    let messages = document.getElementById("messages").value
+
+    console.log(`Your name is ${name}, your email is ${email},
+        your phone number is ${phone}, you chose ${subject}, and your messages is ${messages} `);
+}
