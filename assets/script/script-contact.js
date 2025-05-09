e2ab00e22d45d46145a8952e5db2edf46bd26a86
Hana Fauziah Hanum <hanaanumnnum@gmail.com>
2025-05-09 17:25:49 +0700

    Update tasks notes

--- assets/script/script-contact.js
+++ assets/script/script-contact.js
@@ -9,5 +9,22 @@ function getData(event){
     let messages = document.getElementById("messages").value
 
     console.log(`Your name is ${name}, your email is ${email}, your phone number is ${phone}, you chose ${subject}, and your messages is ${messages} `);
+
+    if (name == ""){
+        return alert("Nama anda masih kosong, silahkan tambahkan nama")
+    }
+    if (email == ""){
+        return alert("Email anda masih kosong, silahkan tambahkan email")
+    }
+    if (phone == ""){
+        return alert("Nomor kontak anda masih kosong, silahkan tambahkan nomor kontak")
+    }
+    if (subject == ""){
+        return alert("Silahkan pilih subyek terlebih dahulu")
+    }
+    if (messages == ""){
+        return alert("Silahkan masukkan pesan anda terlebih dahulu")
+    }
+
 }
 
